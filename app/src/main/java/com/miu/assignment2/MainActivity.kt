ffd7127c858e5aade984715bde4125203c3da0f4
Khosbayar <xocoooo@gmail.com>
2022-10-19 19:44:53 -0500

    Added functionality

--- app/src/main/java/com/miu/assignment2/MainActivity.kt
+++ app/src/main/java/com/miu/assignment2/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.miu.assignment2
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    var foodList: ArrayList<String> =
+        arrayListOf("Hamburger", "Pizza", "Mexican", "American", "Chinese")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun decideFood(view: View) {
+        result_txtv.text = foodList.asSequence().shuffled().find { true }
+    }
+
+    fun addFood(view: View) {
+        foodList.add(add_food_txt.text.toString())
+        add_food_txt.text = null
     }
 }
